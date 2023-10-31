33ef1b89c9a009ffcedc0803ccc02e2cce61670c
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-10-31 15:49:08 +0300

    changes 4

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ContactPage = () => {
   return (
     <div>
-      <h1>THIS IS OUR CONTACT PAGE . </h1>
+      <h1>THIS IS OUR CONTACT  </h1>
     </div>
   )
 }
