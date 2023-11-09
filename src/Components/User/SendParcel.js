7213b3966cdf05506d42e6d7686813bcd6de0681
joseph-kinyuru <josephkinyuru22@gmail.com>
2023-11-09 23:22:59 +0300

    Fixed send parcel

--- src/Components/User/SendParcel.js
+++ src/Components/User/SendParcel.js
@@ -1,32 +1,93 @@
 import React, { useState } from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useNavigate} from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GridLoaderSpinner from "../GridLoader";
 
 const SendParcel = () => {
-  const [parcelName, setParcelName] = useState("");
-  const [receiverName, setReceiverName] = useState("");
-  const [phoneNo, setPhoneNo] = useState("");
-  const [weight, setWeight] = useState("");
-  const [origin, setOrigin] = useState("");
-  const [destination, setDestination] = useState("");
-  const [price, setPrice] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const handleWeightChange = (e) => {
-    const enteredWeight = e.target.value;
-    const rate = 200;
+  const baseURL = "https://parcelpalserver.onrender.com/";
 
-    if (!enteredWeight || isNaN(enteredWeight)) {
-      setWeight("");
-      setPrice(0);
-    } else {
-      const calculatedPrice = parseFloat(enteredWeight) * rate;
-      setWeight(enteredWeight);
-      setPrice(calculatedPrice);
+  const handleTimeout = () => {
+    setIsLoading(false);
+    toast.error("Server did not respond within 15 seconds. Please try again later.");
+  };
+
+  const handleFormSubmit = async (values) => {
+    setIsLoading(true);
+    const user_id = localStorage.getItem('user_id');
+    const price = Math.floor(Math.random() * 3500) + 1;
+    console.log(JSON.stringify({
+      ...values,
+      user: parseInt(user_id), 
+      price: price, 
+      status:"Pending",
+    }),    )
+
+    try {
+      const response = await fetch(`${baseURL}parcels/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...values,
+          user: user_id, 
+          price: price, 
+          status:"Pending",
+        }),      
+      });
+
+      const data = await response.json();
+
+      const timeout = setTimeout(() => {
+        handleTimeout();
+      }, 15000); // 15 seconds
+
+      if (response.ok) {
+        clearTimeout(timeout);
+        setIsLoading(false);
+        toast.success("Parcel submition is successful.", {
+          onClose: () => {
+            setTimeout(() => {
+              navigate("/Dashboard");
+            }, 4000); 
+          },
+        });
+      } else if (response.status === 500) {
+        toast.error("Internal server error. Please try again later.");
+      } else {
+        toast.error(`Something went wrong: ${data.detail}`);
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+      toast.error("Network error. Please try again later.");
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Send the parcel data to the backend or perform any necessary action
-  };
+  const formSchema = yup.object().shape({
+    parcel_name: yup.string().required("Parcel name is required"),
+    destination: yup.string().required("Destination is required"),
+    origin: yup.string().required("Origin is required"),
+    weight: yup.number().required("Weight is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      parcel_name: "",
+      destination: "",
+      origin: "",
+      weight: "",
+    },
+    validationSchema: formSchema,
+    onSubmit: handleFormSubmit,
+  });
 
   return (
     <div className="bg-cover bg-center bg-no-repeat min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url(https://e0.pxfuel.com/wallpapers/321/742/desktop-wallpaper-request-original-xbox-theme-for-new-dashboard-xboxthemes.jpg)' }}>
@@ -66,97 +127,74 @@ const SendParcel = () => {
       <div className="flex justify-center items-center">
         <div className="bg-green-00 rounded-lg shadow-lg p-6 w-1/3">
           <h1 className="text-3xl font-semibold text-center text-green-600 mb-6">Send Parcel</h1>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={formik.handleSubmit}>
             <div className="mb-4">
-              <label htmlFor="parcelName" className="block text-gray-100 text-lg mb-2">
+              <label htmlFor="parcel_name" className="block text-gray-100 text-lg mb-2">
                 Parcel Name
               </label>
               <input
+                id="parcel_name"
+                name="parcel_name"
                 type="text"
-                id="parcelName"
+                onChange={formik.handleChange}
+                value={formik.values.parcel_name}
                 placeholder="Enter Parcel Name"
                 className="w-full px-4 py-2 border border-gray-100rounded-lg focus:outline-none focus:ring focus:ring-green-600 text-lg"
-                value={parcelName}
-                onChange={(e) => setParcelName(e.target.value)}
               />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="receiverName" className="block text-gray-100 text-lg mb-2">
-                Receiver Name
-              </label>
-              <input
-                type="text"
-                id="receiverName"
-                placeholder="Enter Receiver's Name"
-                className="w-full px-4 py-2 border border-gray-100 rounded-lg focus:outline-none focus:ring focus:ring-green-100 text-lg"
-                value={receiverName}
-                onChange={(e) => setReceiverName(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="phoneNo" className="block text-gray-100 text-lg mb-2">
-                Phone Number
-              </label>
-              <input
-                type="text"
-                id="phoneNo"
-                placeholder="Enter Phone Number"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-600 text-lg"
-                value={phoneNo}
-                onChange={(e) => setPhoneNo(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="weight" className="block text-gray-100 text-lg mb-2">
-                Weight (kg)
-              </label>
-              <input
-                type="text"
-                id="weight"
-                placeholder="Enter Weight in kilograms"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-100 text-lg"
-                value={weight}
-                onChange={handleWeightChange}
-              />
+              {formik.touched.parcel_name && formik.errors.parcel_name && (
+                <p className="text-red-500">{formik.errors.parcel_name}</p>
+              )}
             </div>
             <div className="mb-4">
               <label htmlFor="origin" className="block text-gray-100 text-lg mb-2">
                 Origin
               </label>
               <input
-                type="text"
                 id="origin"
+                name="origin"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.origin}
                 placeholder="Enter Origin"
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-100 text-lg"
-                value={origin}
-                onChange={(e) => setOrigin(e.target.value)}
               />
+              {formik.touched.origin && formik.errors.origin && (
+                <p className="text-red-500">{formik.errors.origin}</p>
+              )}
             </div>
             <div className="mb-4">
               <label htmlFor="destination" className="block text-gray-100 text-lg mb-2">
                 Destination
               </label>
-              <input
-                type="text"
+                  <input
                 id="destination"
+                name="destination"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.destination}
                 placeholder="Enter Destination"
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-100 text-lg"
-                value={destination}
-                onChange={(e) => setDestination(e.target.value)}
               />
+              {formik.touched.destination && formik.errors.destination && (
+                <p className="text-red-500">{formik.errors.destination}</p>
+              )}
             </div>
-            <div className="mb-6">
-              <label htmlFor="price" className="block text-gray-100 text-lg mb-2">
-                Price (Ksh)
+            <div className="mb-4">
+              <label htmlFor="weight" className="block text-gray-100 text-lg mb-2">
+                Weight (kg)
               </label>
               <input
-                type="text"
-                id="price"
-                placeholder="Price will be calculated"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-500 text-lg"
-                value={price}
-                readOnly
+                id="weight"
+                name="weight"
+                type="number"
+                onChange={formik.handleChange}
+                value={formik.values.weight}
+                placeholder="Enter Weight in kilograms"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-100 text-lg"
               />
+              {formik.touched.weight && formik.errors.weight && (
+                <p className="text-red-500">{formik.errors.weight}</p>
+              )}
             </div>
             <button
               type="submit"
@@ -165,6 +203,12 @@ const SendParcel = () => {
               Send Parcel
             </button>
           </form>
+          {isLoading && (
+        <div className="overlay">
+          <GridLoaderSpinner />
+        </div>
+      )}
+      <ToastContainer />
         </div>
       </div>
     </div>
--- src/Pages/SendParcel.js
+++ src/Pages/SendParcel.js
@@ -1,163 +1,216 @@
 import React, { useState } from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useNavigate} from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GridLoaderSpinner from "../GridLoader";
 
 const SendParcel = () => {
-  const [parcelName, setParcelName] = useState("");
-  const [receiverName, setReceiverName] = useState("");
-  const [phoneNo, setPhoneNo] = useState("");
-  const [weight, setWeight] = useState("");
-  const [pickup, setPickup] = useState("");
-  const [destination, setDestination] = useState("");
-  const [totalCost, setTotalCost] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const handleWeightChange = (e) => {
-    const enteredWeight = e.target.value;
-    const rate = 200;
+  const baseURL = "https://parcelpalserver.onrender.com/";
 
-    if (!enteredWeight || isNaN(enteredWeight)) {
-      setWeight("");
-      setTotalCost(0);
-    } else {
-      const calculatedCost = parseFloat(enteredWeight) * rate;
-      setWeight(enteredWeight);
-      setTotalCost(calculatedCost);
+  const handleTimeout = () => {
+    setIsLoading(false);
+    toast.error("Server did not respond within 15 seconds. Please try again later.");
+  };
+
+  const handleFormSubmit = async (values) => {
+    setIsLoading(true);
+    const user_id = localStorage.getItem('user_id');
+    const price = Math.floor(Math.random() * 3500) + 1;
+    console.log(JSON.stringify({
+      ...values,
+      user: parseInt(user_id), 
+      price: price, 
+      status:"Pending",
+    }),    )
+
+    try {
+      const response = await fetch(`${baseURL}parcels/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...values,
+          user: user_id, 
+          price: price, 
+          status:"Pending",
+        }),      
+      });
+
+      const data = await response.json();
+
+      const timeout = setTimeout(() => {
+        handleTimeout();
+      }, 15000); // 15 seconds
+
+      if (response.ok) {
+        clearTimeout(timeout);
+        setIsLoading(false);
+        toast.success("Parcel submition is successful.", {
+          onClose: () => {
+            setTimeout(() => {
+              navigate("/Dashboard");
+            }, 4000); 
+          },
+        });
+      } else if (response.status === 500) {
+        toast.error("Internal server error. Please try again later.");
+      } else {
+        toast.error(`Something went wrong: ${data.detail}`);
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+      toast.error("Network error. Please try again later.");
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted");
+  const formSchema = yup.object().shape({
+    parcel_name: yup.string().required("Parcel name is required"),
+    destination: yup.string().required("Destination is required"),
+    origin: yup.string().required("Origin is required"),
+    weight: yup.number().required("Weight is required"),
+  });
 
-    let id = localStorage.getItem('user_id');
-
-    let parcelData = {
-      user :id,
-      parcelName : parcelName,
-      receiver_name : receiverName,
-      receiver_phone_number : phoneNo,
-      weight : weight,
-      origin : pickup, 
-      destination : destination,
-      price : totalCost,
-      status : "Pending"
-
-    }
-    console.log(parcelData)
-    fetch('https://parcelpalserver.onrender.com/parcels/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(parcelData),
-    })
-      .then(r => r.json())
-      .then(data => console.log(data))
-      
-  };
+  const formik = useFormik({
+    initialValues: {
+      parcel_name: "",
+      destination: "",
+      origin: "",
+      weight: "",
+    },
+    validationSchema: formSchema,
+    onSubmit: handleFormSubmit,
+  });
 
   return (
-    <div className="max-w-md mx-auto p-6 bg-yellow-300 rounded-lg shadow-md">
-      <h1 className="text-3xl font-semibold text-center mb-6 text-black">
-        SendParcel
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="parcelName" className="block font-medium text-black mb-2 text-lg">
-            Parcel Name
-          </label>
-          <input
-            type="text"
-            id="parcelName"
-            placeholder="Parcel Name"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 text-lg"
-            value={parcelName}
-            onChange={(e) => setParcelName(e.target.value)}
-          />
+    <div className="bg-cover bg-center bg-no-repeat min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url(https://e0.pxfuel.com/wallpapers/321/742/desktop-wallpaper-request-original-xbox-theme-for-new-dashboard-xboxthemes.jpg)' }}>
+    <header className=" text-white py-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div>
+            <h1 className="text-3xl">
+              <a href="Dashboard">
+                <span id="trackerText">
+                  Parcel<span className="text-orange-600">Pal</span>
+                </span>
+              </a>
+            </h1>
+          </div>
+          <nav className="bg-transparent">
+            <ul className="flex space-x-4 ">
+              <li>
+                <a href="dashboard" className="text-orange-600 text-xl">
+                  User
+                </a>
+              </li>
+              <li>
+                <a href="SendParcel" className="text-orange-600 text-xl">
+                  Parcel
+                </a>
+              </li>
+              <li>
+                <a href="/" className="text-orange-600  text-xl">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className="mb-4">
-          <label htmlFor="receiverName" className="block font-medium text-black mb-2 text-lg">
-            Receiver Name
-          </label>
-          <input
-            type="text"
-            id="receiverName"
-            placeholder="Receiver's name"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 text-lg"
-            value={receiverName}
-            onChange={(e) => setReceiverName(e.target.value)}
-          />
+      </header>
+
+      <div className="flex justify-center items-center">
+        <div className="bg-green-00 rounded-lg shadow-lg p-6 w-1/3">
+          <h1 className="text-3xl font-semibold text-center text-green-600 mb-6">Send Parcel</h1>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="parcel_name" className="block text-gray-100 text-lg mb-2">
+                Parcel Name
+              </label>
+              <input
+                id="parcel_name"
+                name="parcel_name"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.parcel_name}
+                placeholder="Enter Parcel Name"
+                className="w-full px-4 py-2 border border-gray-100rounded-lg focus:outline-none focus:ring focus:ring-green-600 text-lg"
+              />
+              {formik.touched.parcel_name && formik.errors.parcel_name && (
+                <p className="text-red-500">{formik.errors.parcel_name}</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <label htmlFor="origin" className="block text-gray-100 text-lg mb-2">
+                Origin
+              </label>
+              <input
+                id="origin"
+                name="origin"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.origin}
+                placeholder="Enter Origin"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-100 text-lg"
+              />
+              {formik.touched.origin && formik.errors.origin && (
+                <p className="text-red-500">{formik.errors.origin}</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <label htmlFor="destination" className="block text-gray-100 text-lg mb-2">
+                Destination
+              </label>
+                  <input
+                id="destination"
+                name="destination"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.destination}
+                placeholder="Enter Destination"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-100 text-lg"
+              />
+              {formik.touched.destination && formik.errors.destination && (
+                <p className="text-red-500">{formik.errors.destination}</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <label htmlFor="weight" className="block text-gray-100 text-lg mb-2">
+                Weight (kg)
+              </label>
+              <input
+                id="weight"
+                name="weight"
+                type="number"
+                onChange={formik.handleChange}
+                value={formik.values.weight}
+                placeholder="Enter Weight in kilograms"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-100 text-lg"
+              />
+              {formik.touched.weight && formik.errors.weight && (
+                <p className="text-red-500">{formik.errors.weight}</p>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="bg-green-700 text-white px-4 py-2 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-green-900 text-lg"
+            >
+              Send Parcel
+            </button>
+          </form>
+          {isLoading && (
+        <div className="overlay">
+          <GridLoaderSpinner />
         </div>
-        <div className="mb-4">
-          <label htmlFor="phoneNo" className="block font-medium text-black mb-2 text-lg">
-            Phone No
-          </label>
-          <input
-            type="text"
-            id="phoneNo"
-            placeholder="Phone number"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 text-lg"
-            value={phoneNo}
-            onChange={(e) => setPhoneNo(e.target.value)}
-          />
+      )}
+      <ToastContainer />
         </div>
-        <div className="mb-4">
-          <label htmlFor="weight" className="block font-medium text-black mb-2 text-lg">
-            Weight (kg)
-          </label>
-          <input
-            type="text"
-            id="weight"
-            placeholder="Weight in kilograms"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 text-lg"
-            value={weight}
-            onChange={handleWeightChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="pickup" className="block font-medium text-black mb-2 text-lg">
-            Pickup Location
-          </label>
-          <input
-            type="text"
-            id="pickup"
-            placeholder="Pickup location"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 text-lg"
-            value={pickup}
-            onChange={(e) => setPickup(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="destination" className="block font-medium text-black mb-2 text-lg">
-            Destination Location
-          </label>
-          <input
-            type="text"
-            id="destination"
-            placeholder="Destination location"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 text-lg"
-            value={destination}
-            onChange={(e) => setDestination(e.target.value)}
-          />
-        </div>
-        <div className="mb-6">
-          <label htmlFor="totalCost" className="block font-medium text-black mb-2 text-lg">
-            Total Cost (Ksh)
-          </label>
-          <input
-            type="text"
-            id="totalCost"
-            placeholder="Total cost will be calculated"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 text-lg"
-            value={totalCost}
-            readOnly
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-black text-yellow-500 px-4 py-2 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-yellow-500 text-lg"
-        >
-          Send Parcel
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
