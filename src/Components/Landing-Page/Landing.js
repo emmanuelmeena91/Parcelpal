b84309f1501c67ce191c9719a657a538cc879370
gilbert <gilbert.bageni@student.moringaschool.com>
2023-11-09 05:02:28 +0300

    Made changes

--- src/Components/Landing-Page/Landing.js
+++ src/Components/Landing-Page/Landing.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Landing = () => {
+  const backgroundStyles = {
+    backgroundImage: 'url("https://img.lovepik.com/background/20211022/medium/lovepik-light-effect-atmospheric-gold-powder-background-image_605806949.jpg")',
+    backgroundSize: 'cover',
+    backgroundPosition: '',
+    height: '90vh', // Make sure the background covers the full viewport height
+  };
+
+  const contentStyles = {
+    backgroundColor: ')', // Set the background color with transparency
+  };
+
   return (
-    <div>
-      <div className="bg-gray-300 min-h-screen">
-        <section className="py-16 bg-gray-200">
-          <div className="container mx-auto px-4">
-            <h2 className="text-4xl font-semibold text-center mb-4">Welcome to ParcelPal Delivery</h2>
-            <div className="flex justify-center">
-              <Link to="/">
-                <button className="bg-orange-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
-                  Get Started
-                </button>
-              </Link>
-            </div>
+    <div style={backgroundStyles}>
+      <section className="py-16" style={contentStyles}>
+        <div className="container mx-auto px-4">
+          <h2 className="text-5xl font-semibold text-center mb-4 text-orange-500">Welcome to ParcelPal Delivery</h2>
+          <div className="flex justify-center">
+            <Link to="/">
+              <button className="bg-orange-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                Get Started
+              </button>
+            </Link>
           </div>
-        </section>
-
-
-      </div>
+        </div>
+      </section>
     </div>
   );
 };
